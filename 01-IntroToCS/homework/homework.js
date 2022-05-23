84aa5cc1d03663268493bc3f843bdf5f2ecf6917
aleema95 <alejandro.manouellian@gmail.com>
2022-05-23 16:32:06 -0300

    first commit y primer ejercicio resuelto

---
+++ 01-IntroToCS/homework/homework.js
@@ -0,0 +1,32 @@
+'use strict'
+
+function BinarioADecimal(num) {
+  // tu codigo aca
+  var arr = num.split('')
+  var rev = arr.reverse();
+  var suma = 0;
+  let i = 0;
+  
+    for(i=0;i<rev.length;i++) {
+       suma = suma + rev[i]*(2**i);
+    }
+    console.log(suma);
+    return suma
+}
+
+function DecimalABinario(num) {
+  // tu codigo aca
+  var arr = [];        
+        while (num >= 1) {
+            arr.push(Math.floor(num%2))
+            num = num/2
+        }
+        arr.reverse()
+        return arr.join('');
+        
+}
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
