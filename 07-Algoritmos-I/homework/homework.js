cef6a12b759517ff47092194ae31095d429ae530
aleema95 <alejandro.manouellian@gmail.com>
2022-05-31 13:30:11 -0300

    ejercicios 07 resueltos

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,7 +6,17 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  let array = [1];
+  let i = 2;
+  while (num !== 1) {
+    if(num%i === 0){
+      array.push(i)
+      num = num/i
+    } else {
+      i++
+    }
+  }
+  return array;
 }
 
 function bubbleSort(array) {
@@ -14,7 +24,36 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  let a = 0;
+  let b = 0;  
 
+    for(let i = 0; i < array.length; i++){
+      for (let j = i + 1; j < array.length; j++) {
+        if(array[i] > array[j]){
+            a = array[i];
+            b = array[j];
+          array[i] = b;
+          array[j] = a;
+        }
+      }
+    }
+    console.log(array);
+   return array
+
+
+    // solucion de martina--------------------------------------------------------
+  //  let swap = true;
+  // while (swap) {
+  //   swap = false;
+  //   for (let i = 0; i < array.length; i++) {
+  //     if (array[i] > array[i+1]) {
+  //       let aux = array[i];
+  //       array[i] = array[i+1];
+  //       array[i+1] = aux;
+  //       swap = true;
+  //     }
+  //   }
+  // }
 }
 
 
@@ -23,17 +62,38 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for(let i = 1; i < array.length; i++){
+    let j = i - 1;
+    let vg = array[i];
+    while (j >= 0 && vg < array[j]) {
+      array[j + 1] = array[j];
+      j--        
+    }
+    array[j + 1] = vg;
+  }
+  return array
 }
-
 
 function selectionSort(array) {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
-}
+   for (let i = 0; i < array.length - 1; i++) {
+     let min = i;
+     for (let j = i + 1; j < array.length; j++){
+       if (array[min] > array[j]){
+         min = j;      
+       }
+     }   
+     if(i !== min){
+       let vg = array[i];
+       array[i] = array[min];
+       array[min] = vg;
+     }
+    }
+    return array;    
+} 
 
 
 // No modificar nada debajo de esta línea
